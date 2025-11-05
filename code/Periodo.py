e62ab20ad4936f3a98dc043b601eaa6abaa17f91
Vinicius-O-Ferraz <vinicius.f.ferraz.bsi@gmail.com>
2025-11-05 13:44:28 -0300

    feat: implementation of period and clases class

---
+++ code/Periodo.py
@@ -0,0 +1,11 @@
+from Turma import turma
+
+class periodo:
+    def __init__(self):
+        self.matriz = [[0 for _ in range(5)] for _ in range(2)]
+
+    def __repr__(self):
+        return "\n".join(str(linha) for linha in self.matriz)
+
+periodo1 = periodo()
+print(periodo1)
---
+++ code/Turma.py
@@ -0,0 +1,12 @@
+class turma:
+    def __init__(self, id_turma, professor, horarios, semestre_letivo):
+        self.id_turma = id_turma
+        self.horarios = horarios  
+        self.professor = professor
+        self.semestre_letivo = semestre_letivo
+
+    def __repr__(self):
+        return f"{self.id_turma}"
+    
+turma1 = turma("TAVIA", "Prof. Silva", '23T45', "2024.1")
+print(turma1)
